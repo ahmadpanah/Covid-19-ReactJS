831ff1875e04e76390159697a3343ca1c8e3b709
Hossein <djalicrt@gmail.com>
2021-05-01 17:54:20 +0430

    Fetch Country Names From API Show them in Native Select

--- src/api/index.js
+++ src/api/index.js
@@ -26,12 +26,23 @@ export const fetchDailyData = async () => {
             date: dailyData.reportDate,
         }));
 
-        console.log(data);
-
         return modifiedData;
         
     } catch (error) {
+        console.log(error);
+    }
+}
+
+export const fetchCountries = async () => {
+
+    try {
+        const { data: {countries}} = await axios.get(`${url}/countries`);
         
+        return countries.map((country) => country.name);
+
+        } catch (error) {
+        console.log(error);
     }
 
 }
+
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import styles from './CountryPicker.module.css';
+
+import { fetchCountries } from '../../api';
 
 const CountryPicker = () => {
+
+    const [fetchedCountries , setfetchedCountries] = useState([]);
+
+    useEffect(()=> {
+        const fetchAPI = async () => {
+            setfetchedCountries(await fetchCountries());
+        }
+
+        fetchAPI();
+    },[setfetchedCountries]);
+
+
     return (
-        <h1>CountryPicker!</h1>
+       <FormControl className={styles.FormControl}>
+           <NativeSelect>
+               <option value="global">World</option>
+                {fetchedCountries.map((country , i) => <option key={i} value={country}>{country}</option> )};
+           </NativeSelect>
+       </FormControl>
     )
 }
 
